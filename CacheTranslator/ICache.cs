d611c2e9829956c32596742f89c16b7a12876ca7
mwroblewski <milwroblewski@gmail.com>
2022-03-02 19:00:20 +0100

    Added ICache

---
+++ CacheTranslator/ICache.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CacheTranslator
+{
+    public interface ICache
+    {
+        List<BlockRange> GetFileMapping(FileId fileIdentification);
+        void CacheBlocks(List<BlockRange> blocks);
+        void EvictBlocks(List<BlockRange> blocks);
+    }
+}
