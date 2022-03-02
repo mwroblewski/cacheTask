903b849442392e13080dba48fbe2d568a50b6868
mwroblewski <milwroblewski@gmail.com>
2022-03-02 19:21:45 +0100

    FirmwareCache class added

---
+++ CacheTranslator/FirmwareCache.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace CacheTranslator
+{
+    internal class FirmwareCache : ICache
+    {
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern void CacheBlocks(List<BlockRange> blocks);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern void EvictBlocks(List<BlockRange> blocks);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern List<BlockRange> GetFileMapping(FileId fileIdentification);
+    }
+}
