0837f21bb079679284775f1544248a22b1f59ee0
mwroblewski <milwroblewski@gmail.com>
2022-03-02 19:08:00 +0100

    Added ICacheTranslator and CacheTranslator

---
+++ CacheTranslator/Cache/CacheTranslator.cs
@@ -0,0 +1,20 @@
+﻿namespace CacheTranslator
+{
+    public class CacheTranslator : ICacheTranslator
+    {
+        public void Add(FileId fileIdentification)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(FileId fileIdentification)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Synchronize(FileId fileIdentification)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CacheTranslator/Cache/ICacheTranslator.cs
@@ -0,0 +1,9 @@
+﻿namespace CacheTranslator
+{
+    public interface ICacheTranslator
+    {
+        void Add(FileId fileIdentification);
+        void Remove(FileId fileIdentification);
+        void Synchronize(FileId fileIdentification);
+    }
+}
