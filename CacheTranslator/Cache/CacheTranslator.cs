c92c983afa9c501651e1e9d90c809c28309f64dc
mwroblewski <milwroblewski@gmail.com>
2022-03-02 19:43:33 +0100

    CacheTranslator - Add and Remove - in progress

--- CacheTranslator/Cache/CacheTranslator.cs
+++ CacheTranslator/Cache/CacheTranslator.cs
@@ -2,14 +2,31 @@
 {
     public class CacheTranslator : ICacheTranslator
     {
+        private readonly ICache _cache;
+        private readonly IPersistence _persistence;
+
+        public CacheTranslator(ICache cache, IPersistence persistence)
+        {
+            _cache = cache;
+            _persistence = persistence;
+        }
+
         public void Add(FileId fileIdentification)
         {
-            throw new System.NotImplementedException();
+            if (_persistence.Contains(fileIdentification))
+            {
+                return;
+            }
+            var blocks = _cache.GetFileMapping(fileIdentification);
+            _cache.CacheBlocks(blocks);
+            _persistence.Add(fileIdentification, blocks);
         }
 
         public void Remove(FileId fileIdentification)
         {
-            throw new System.NotImplementedException();
+            Synchronize(fileIdentification);
+            _persistence.Remove(fileIdentification);
+
         }
 
         public void Synchronize(FileId fileIdentification)
